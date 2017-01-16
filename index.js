f5e92c661b030d08b9d6d80a4031042c990cf968
Rudy Pena <penar@us.ibm.com>
2017-01-16 16:01:59 -0600

    feat(modifying): modifying image through opencv methods/api

---
+++ index.js
@@ -0,0 +1,34 @@
+const cv = require('opencv');
+
+cv.readImage('img/opencv-loaded-image.jpg', function(err, img) {
+  // The loaded image is an Object
+  // It represents the Basic Data Structure to work with in OpenCV - Matrix
+  // Matrix, where one field is one pixel of the image
+  // Matrix Size is defined by the size of the loaded image
+
+  if (err) {
+    throw err;
+  }
+
+  const width = img.width();
+  const height = img.height();
+
+  if (width < 1 || height < 1) {
+    throw new Error('Image has no size.');
+  }
+
+  // do cool stuff
+  // Grey Scale
+  // img.convertGrayscale();
+
+  // Red Background
+  // img.convertHSVscale()
+
+  // Cropping Image
+  var newImg = img.copy();
+  croppedImg = newImg.crop(500,500,500,500);
+  copyCroppedImg.save('./img/croppedImage.jpg');
+
+  // save img
+  img.save('./img/myNewImage.jpg');
+});
